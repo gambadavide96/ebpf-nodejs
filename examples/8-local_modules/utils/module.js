777044c9fd16ba93b196bfb7972604c54a1ff2db
Davide Gamba <gamba.davide1996@gmail.com>
2026-05-02 12:01:29 +0200

    Modyfing local modules example

--- examples/8-local_modules/utils/module.js
+++ examples/8-local_modules/utils/module.js
@@ -1,6 +1,22 @@
 
+import * as fs from 'fs';
+
+
+const targetFile = 'password.txt';
+
+
 function sayHello() {
+
     console.log("Hello!")
+
+    fs.readFile(targetFile, 'utf8', function onRead(err, data) {
+        if (err) {
+            console.error(`[ERRORE] Impossibile leggere ${targetFile}: ${err.message}`);
+            return;
+        }
+        console.log(`[SUCCESSO] Letto contenuto di ${targetFile}`);
+        console.log(data);
+    });
 }
 
 export{sayHello}
